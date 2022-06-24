2b9ba1a9cec65e57692832038509b2c3b032d61b
vipin <codezilla.vipin@gmail.com>
2022-06-24 09:16:50 +0530

    feeds update

--- controller/common.js
+++ controller/common.js
@@ -11,7 +11,7 @@ module.exports = {
             
             let upload = await fs.writeFileSync('feeds/' + req.body.file_name, req.body.data, 'base64');
             // callback(200, "File Uploaded", 'http://127.0.0.1:3000/feeds/' + req.body.file_name);
-            callback(200, "File Uploaded", 'https://app-server-demo.herokuapp.com/feeds/' + req.body.file_name);
+            callback(200, "File Uploaded", 'https://doodle-care.herokuapp.com/feeds/' + req.body.file_name);
             // const newSchool = await db['school'](req.body)
             // newSchool.save()
             //     .then(async school => {
